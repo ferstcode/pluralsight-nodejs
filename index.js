8375f6eee34cc028f70b40dcb9f5b9c4bfdf9ed7
ferstcode <katheryne.arancib.12@sansano.usm.cl>
2020-01-04 17:55:32 -0300

    add ejs component

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/',function(request, response) {
 
-    var tweets = [{text: "hola po"}, {test: "cuando nos vemos"}]
+    var tweets = [{text: "hola po"}, {text: "cuando nos vemos"}]
     response.sendFile(__dirname + "/index.html");
 });
 app.listen(8080);
