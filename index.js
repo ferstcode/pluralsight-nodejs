b0db892bd5ae6417924701c4cc8055ea670d3bf6
ferstcode <katheryne.arancib.12@sansano.usm.cl>
2020-01-04 18:41:05 -0300

    add endpoint tweets for default

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(request, response) {
+app.get('/tweets/:username',function(req, response) {
 
-    var tweets = [{text: "hola po"}, {text: "cuando nos vemos"}]
-    response.sendFile(__dirname + "/index.html");
+    var tweets = [{text: "hola po"}, {text: "cuando nos vemos"}];
+    var username = req.params.username; 
+    response.locals = {tweets: tweets, name: username};
+    response.render('tweets.ejs');
 });
 app.listen(8080);
